81f9776e964a56b249dc424112d204ef3fb5481d
Aditya <adityabasak7031@gmail.com>
2024-03-06 16:35:32 +0530

    make it responsive for 768px

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Enthusiast Lerner', 'Software Developer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+const socialLinks = document.querySelector('.social');
+
+menuToggle.addEventListener('click', () => {
+    document.body.classList.toggle('menu-open');
+});
